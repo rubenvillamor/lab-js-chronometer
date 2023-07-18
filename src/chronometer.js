5946f4cd58832dabbdcf6f76e8e247ef971d9318
Ruben <villamorlage@gmail.com>
2023-07-18 17:58:20 +0200

    done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,47 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    if (typeof printTimeCallback === "function") {
+      this.intervalId = setInterval(() => {
+        this.currentTime++;
+        printTimeCallback(this.currentTime);
+      }, 1000);
+    } else {
+      this.intervalId = setInterval(() => {
+        this.currentTime++;
+      }, 1000);
+    }
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    const stringValue = value.toString();
+    if (stringValue.length < 2) {
+      return "0" + stringValue;
+    } else {
+      return stringValue;
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
+    this.intervalId = null;
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
-  split() {
-    // ... your code goes here
-  }
+  split() {}
 }
